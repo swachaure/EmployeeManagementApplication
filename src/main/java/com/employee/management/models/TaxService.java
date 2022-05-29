0e419bac530ed62e1fab7dce7ba916c3c5a2286d
swachaure <62742870+swachaure@users.noreply.github.com>
2022-05-30 00:47:10 +1000

    third commit

--- src/main/java/com/employee/management/models/TaxService.java
+++ src/main/java/com/employee/management/models/TaxService.java
@@ -1,33 +1,54 @@
 package com.employee.management.models;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Locale;
 
 import javax.money.CurrencyUnit;
 import javax.money.Monetary;
 
 import org.javamoney.moneta.Money;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
 @Service
 public class TaxService {
 
 	public double CalculateIncomeTax(int salary) {
+		String taxJsonString = "";
+		try {
+			String name = new File(".").getCanonicalPath();
+			File file = new File(name, "/" + "tax_brackets.json");
+
+			// Creating an object of BufferedReader class
+			BufferedReader br = new BufferedReader(new FileReader(file));
+
+			String str;
+
+			while ((str = br.readLine()) != null)
+				taxJsonString = taxJsonString + str;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		JSONArray taxJsonArray = new JSONArray(taxJsonString);
 		CurrencyUnit usd = Monetary.getCurrency(Locale.US);
-		if (salary <= 37000 && salary >= 18201) {
-			Money money = Money.of(salary - 18200, usd);
-			money = money.multiply(0.19).divideToIntegralValue(12);
-			return money.getNumber().doubleValue();
-		} else if (salary <= 87000 && salary >= 37001) {
-			Money money = Money.of(salary - 37000, usd);
-			money = money.multiply(0.325).add(Money.of(3572, usd)).divideToIntegralValue(12);
-			return money.getNumber().doubleValue();
-		} else if (salary <= 180000 && salary >= 87001) {
-			Money money = Money.of(salary - 87000, usd);
-			money = money.multiply(0.37).add(Money.of(19822, usd)).divideToIntegralValue(12);
-			return money.getNumber().doubleValue();
-		} else if (salary >= 180001) {
-			Money money = Money.of(salary - 180000, usd);
-			money = money.multiply(0.45).add(Money.of(54232, usd)).divideToIntegralValue(12);
-			return money.getNumber().doubleValue();
+		for (int i = 0; i < taxJsonArray.length(); i++) {
+
+			JSONObject obj = taxJsonArray.getJSONObject(i);
+			Double min = Double.parseDouble((String) obj.get("min"));
+			Double max = Double.parseDouble((String) obj.get("max"));
+			Double additionalAmount = Double.parseDouble((String) obj.get("additional_amount"));
+			Double taxPercentage = Double.parseDouble((String) obj.get("tax_percent_multiplier"));
+
+			if (salary >= min && salary <= max) {
+				Money money = Money.of(salary - (min != 0 ? min - 1 : 0), usd);
+				money = money.multiply(taxPercentage).add(Money.of(additionalAmount, usd)).divideToIntegralValue(12);
+				return money.getNumber().doubleValue();
+			}
 		}
 		return 0D;
 	}
